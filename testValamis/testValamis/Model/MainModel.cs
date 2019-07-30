27391d136c03cca975010579470d7a0e6ddf4051
DanTe Vergil <danteatom1c@gmail.com>
2019-07-30 16:19:26 +0300

    Selenium Test

---
+++ testValamis/testValamis/Model/MainModel.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace testValamis.Model
+{
+    class MainModel : BindableBase
+    {
+        private IWebElement webElement;
+
+        public void StartTest(string newUrl)
+        {
+            RemoteWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl(newUrl);
+            webElement=webDriver.FindElementById("user-addr__input");
+            webElement.SendKeys("Петрозаводск, улица Германа Титова, 11");
+            webElement = webDriver.FindElementById("user-addr__form");
+        }
+    }
+}
---
+++ testValamis/testValamis/ViewModel/MainVM.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testValamis.Model;
+
+namespace testValamis.ViewModel
+{
+    class MainVM : BindableBase
+    {
+
+        private string myUrl = "https://www.delivery-club.ru/";
+        readonly MainModel mainModel = new MainModel();
+        public MainVM()
+        {
+            mainModel.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+            StartTest = new DelegateCommand<string>(str => { mainModel.StartTest(myUrl); });
+        }
+        public DelegateCommand<string> StartTest { get; }
+        public string UrlSite
+        {
+            get { return myUrl; }
+        }
+    }
+}
